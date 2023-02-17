a8a1e217ff73990a1de2ffce0084915916916205
victor_workspace <victor@aws_workspace>
2023-02-17 16:40:13 -0600

    update notes + code

--- map_comparison/map_comparison.cc
+++ map_comparison/map_comparison.cc
@@ -4,6 +4,9 @@
 /////////////////////////////////////////
 // INCLUDE NECESSARY HEADER FILES HERE //
 /////////////////////////////////////////
+#include<map>
+#include<string>
+#include<unordered_map>
 
 using namespace std;
 
@@ -14,18 +17,25 @@ void func_map (vector<pair<int,string>>& items)
   ////////////////////////////////////////////////
   // CREATE AN EMPTY map MAPPING int TO string. //
   ////////////////////////////////////////////////
+  map<int, string> myMap;
 
 
   ////////////////////////////////////////////
   // USING A RANGE-BASED for(...) LOOP,     //
   // INSERT EACH ITEM IN items INTO THE MAP //
   ////////////////////////////////////////////
+  for(auto i=0; i<items.size(); i++){
+    myMap.insert(pair<int,string>(items[i]));
+  }
 
 
   //////////////////////////////////////////////
   // USING A RANGE-BASED for(...) LOOP,       //
   // PRINT OUT EACH KEY-VALUE PAIR IN THE MAP //
   //////////////////////////////////////////////
+  for(auto &i : myMap){
+    cout << "[" <<  i.first << ", " << i.second << "] \n";
+  }
 
 
   cout << endl << "=== END func_map ===" << endl;
@@ -38,6 +48,7 @@ void func_unordered_map (vector<pair<int,string>>& items)
   //////////////////////////////////////////////////////////
   // CREATE AN EMPTY unordered_map MAPPING int TO string. //
   //////////////////////////////////////////////////////////
+  unordered_map<int,string> randomMap; 
 
 
   /////////////////////////////////////////////
@@ -50,12 +61,19 @@ void func_unordered_map (vector<pair<int,string>>& items)
   // IN THE FOLLOWING FORMAT:                //
   //   o  "[N,B,LF] = [3,10,0.4432]"         //
   /////////////////////////////////////////////
-
+  for(auto i=0; i<items.size(); i++){
+    randomMap.insert(pair<int, string>(items[i])); 
+    cout << "[N,B,LF] = [" << randomMap.size() << "," << randomMap.bucket_count() << "," << randomMap.load_factor() << "]" << endl;
+  }
+  
 
   ///////////////////////////////////////////////
   // USING A RANGE-BASED for(...) LOOP,        //
   // PRINT OUT EACH KEY-VALUE PAIR IN THE MAP. //
   ///////////////////////////////////////////////
+  for(auto &i : randomMap){
+    cout << "[" << i.first << ", " << i.second << "] \n";
+  }
 
 
   cout << endl << "=== END func_unordered_map ===" << endl;
